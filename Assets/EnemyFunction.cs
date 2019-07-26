2e352f8a7771e2e530c5840b384a2d5ca9f51998
OhMyBug <sa0963278610@gmail.com>
2019-07-26 16:33:39 +0800

    Unity

---
+++ Assets/EnemyFunction.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyFunction : MonoBehaviour
+{
+    public GameObject explode;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameObject.transform.position += new Vector3(0, -0.01f, 0);
+    }
+
+    void OnTriggerEnter2D (Collider2D col) {
+      if(col.tag == "Bullet" || col.tag == "Player"){
+        Destroy(col.gameObject);
+        Destroy(gameObject);
+
+        Instantiate(explode, transform.position, transform.rotation);
+        if(col.tag == "Player") {
+            Instantiate(explode, col.gameObject.transform.position, col.gameObject.transform.rotation);
+            GameFunction.Instance.GameOver();
+        }
+
+        GameFunction.Instance.AddScore();
+      }
+    }
+}
---
+++ Assets/ShipControl.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipControl : MonoBehaviour
+{
+    public GameObject Bullet;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            gameObject.transform.position += new Vector3(0.1f, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            gameObject.transform.position += new Vector3(-0.1f, 0, 0);
+        }
+        if(Input.GetKey(KeyCode.UpArrow))
+        {
+            gameObject.transform.position += new Vector3(0, 0.1f, 0);
+        }
+        if(Input.GetKey(KeyCode.DownArrow))
+        {
+            gameObject.transform.position += new Vector3(0, -0.1f, 0);
+        }
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+          Vector3 pos = gameObject.transform.position + new Vector3(0, 0.6f, 0);
+          Instantiate(Bullet, pos, gameObject.transform.rotation);
+        }
+    }
+}
